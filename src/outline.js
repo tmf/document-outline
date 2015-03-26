d7678506858cf7f532273c2b2f3fbf4fe0d392d2
tmf <tom.forrer@gmail.com>
2015-03-26 13:24:32 +0100

    refactor outline parser for modularity
    added amd and commonjs wrapper for module, refactored to module,
    separated parsing and dom generation

--- src/outline.js
+++ src/outline.js
@@ -1,31 +1,70 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var outline = document.createElement('ul'),
-        currentLevel = 1,
-        currentElement = outline,
-        lastElement = null;
-    [].forEach.call(document.querySelectorAll('h1, h2, h3, h4, h5, h6'), function (heading) {
-        var el = document.createElement('li');
-        el.textContent = heading.textContent;
+/*!
+ * docoument-outline - a TOC generator
+ * https://github.com/tmf/document-outline
+ * MIT License | (c) Tom Forrer 2015
+ */
+// amd and commonjs support from http://ifandelse.com/its-not-hard-making-your-library-support-amd-and-commonjs/
+(function (root, factory) {
+    if(typeof define === "function" && define.amd) {
+        define([], function(){
+            return (root.documentOutline = factory());
+        });
+    } else if(typeof module === "object" && module.exports) {
+        module.exports = (root.documentOutline = factory());
+    } else {
+        root.documentOutline = factory();
+    }
+}(this, function() {
+    var documentOutline = {
+        /**
+         * [
+         *   {
+         *     title: "heading 1",
+         *     ref: nodeRef,
+         *     elements: [
+         *       {
+         *         title: "heading 2",
+         *         ref: nodeRef,
+         *         elements: [ ]
+         *       }
+         *     ]
+         *   }
+         * ]
+         * @param containerNode
+         */
+        extractOutline: function(containerNode){
+            var outline = [],
+                currentLevel = 1,
+                currentSection = outline,
+                previousElement = null,
+                parentSections = [];
+            [].forEach.call(containerNode.querySelectorAll('h1, h2, h3, h4, h5, h6'), function(headingNode){
+                var title = headingNode.textContent,
+                    level = headingNode.hasAttribute('aria-level') ? parseInt(headingNode.getAttribute('aria-level')) : parseInt(headingNode.nodeName[1]),
+                    element = { title: title, ref: headingNode, elements: [] };
+                if(level > currentLevel){
+                    parentSections.push(currentSection);
+                    currentSection = previousElement.elements;
+                }else if(level < currentLevel){
+                    for(var i=0; i<(currentLevel - level); i++){
+                        currentSection = parentSections.pop()
+                    }
+                }
+                currentLevel = level;
+                currentSection.push(element);
+                previousElement = element;
+            });
+            return outline;
+        },
+        /**
+         *
+         * @param outline
+         */
+        createTOC: function(outline){
+            outline.map(function(element){
 
-        if (heading.nodeName[1] > currentLevel) {
-            var ul = document.createElement('ul');
-            ul.appendChild(el);
-            lastElement.appendChild(ul);
-            currentLevel = heading.nodeName[1];
-            currentElement = ul;
-
-        } else if (heading.nodeName[1] == currentLevel) {
-            currentElement.appendChild(el);
-        } else {
-            for (var i = 1; i <= ( currentLevel - heading.nodeName[1]); i++) {
-                currentElement = currentElement.parentNode.parentNode;
-            }
-            currentElement.appendChild(el);
-            currentLevel = heading.nodeName[1];
+            })
         }
-        lastElement = el;
-    });
-
-    document.querySelector('#toctitle').appendChild(outline);
-
-});
+    };
+    return documentOutline;
+}));
